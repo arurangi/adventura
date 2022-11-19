3a16dbaaf870f681fbe34fc2ee955382319cb85b
Arsene Rurangiza <arurangi@fu-r7-p8.s19.be>
2022-11-19 17:21:40 +0100

    updated

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 11:36:54 by arurangi          #+#    #+#             */
-/*   Updated: 2022/11/19 15:46:39 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/11/19 16:32:38 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,18 @@ int	main(void)
 {
 	void	*mlx_ptr;
 	void	*mlx_win;
+	int		height;
+	int		width;
+
+	void	*img_data;
 	
 	mlx_ptr = mlx_init();
 	mlx_win = mlx_new_window(mlx_ptr, 1920, 1080, "Video Game");
 	
-	mlx_pixel_put(mlx_ptr, mlx_win, 50, 50, 045);
+	img_data = mlx_xpm_file_to_image(mlx_ptr, "square-monster.xpm", &width, &height);
+	width = 1;
+	height = 1;
+	mlx_put_image_to_window(mlx_ptr, mlx_win, img_data, 0, 0);
 	mlx_loop(mlx_ptr);
 	
 	return (0);
