42c062a17ab9157657a0a1b2b9642862e391a211
Arsene Rurangiza <arurangi@fu-r7-p5.s19.be>
2022-12-01 14:31:19 +0100

    updated

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 12:29:26 by Arsene            #+#    #+#             */
-/*   Updated: 2022/12/01 11:23:52 by arurangi         ###   ########.fr       */
+/*   Updated: 2022/12/01 14:29:02 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ int	render(t_game *game)
 	if (game->window == NULL)
         return (1);
     
-    render_background(&game->empty_space, encode_rgb(255, 255, 255));
+    render_background(&game->empty_space, encode_rgb(255, 255, 200));
     
+    // Background
+    render_rect(&game->img, (t_rect){0, 0,
+		W_WIDTH, W_HEIGHT, encode_rgb(255, 255, 255)});
+    // Green
     render_rect(&game->img, (t_rect){W_WIDTH - 100, W_HEIGHT - 100,
 		100, 100, encode_rgb(85, 208, 81)});
+    // Red
     render_rect(&game->img, (t_rect){0, 0, 500, 300, encode_rgb(255, 0, 0)});
 
     mlx_put_image_to_window(game->mlx, game->window, game->img.mlx_img, 0, 0);
